681aa7576c79ac3ac3b1b079a07b31cd788dcf6f
Yoshihiro Yamazaki <yoya@awm.jp>
2024-10-21 02:38:45 +0900

    どのフォルダから実行しても良いよう改良。

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
-for m in `ls .git/modules`;
-do echo $m ;
-   git submodule update --remote $m
-   git commit $m -m "update $m" && git push;
-done
+#! /bin/bash -euo pipefail
+
+dir=$(cd $(dirname $0); pwd)
+
+( cd $dir ; 
+  for m in `ls .git/modules`;
+  do echo $m ;
+     git submodule update --remote $m
+     git commit $m -m "update $m" && git push;
+  done
+)
